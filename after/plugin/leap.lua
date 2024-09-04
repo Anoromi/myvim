499409d9cdd0479a6da190e87c00dc557a058e65
Anoromi <and.zagorulko@gmail.com>
2024-09-04 12:43:37 +0300

    add leap nvim

---
+++ after/plugin/leap.lua
@@ -0,0 +1,3 @@
+vim.keymap.set({'n', 'x', 'o'}, 'f',  '<Plug>(leap-forward)')
+vim.keymap.set({'n', 'x', 'o'}, 'F',  '<Plug>(leap-backward)')
+vim.keymap.set({'n', 'x', 'o'}, 'gf', '<Plug>(leap-from-window)')
--- lua/andrii/lazy.lua
+++ lua/andrii/lazy.lua
@@ -195,11 +195,14 @@ require("lazy").setup({
     --}
     {
       "vhyrro/luarocks.nvim",
-      priority = 1000,                        -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+      priority = 1000,                          -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
       opts = {
         rocks = { "lunajson", "pathlib.nvim" }, -- specifies a list of rocks to install
         -- luarocks_build_args = { "--with-lua=/my/path" }, -- extra options to pass to luarocks's configuration script
       },
+    },
+    {
+      "ggandor/leap.nvim"
     }
 
   },
