24cedbdc254e980d3464790bfb2604d4b0cd8186
Anoromi <and.zagorulko@gmail.com>
2024-09-29 10:12:49 +0300

    switch to vscode theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,54 +1,33 @@
---require('rose-pine').setup({
---	--- @usage 'auto'|'main'|'moon'|'dawn'
---	variant = 'dawn',
---	--- @usage 'main'|'moon'|'dawn'
---	dark_variant = 'main',
---	bold_vert_split = false,
---	dim_nc_background = false,
---	disable_background = false,
---	disable_float_background = false,
---	disable_italics = false,
---
---	--- @usage string hex value or named color from rosepinetheme.com/palette
---	groups = {
---		background = 'base',
---		background_nc = '_experimental_nc',
---		panel = 'surface',
---		panel_nc = 'base',
---		border = 'highlight_med',
---		comment = 'muted',
---		link = 'iris',
---		punctuation = 'subtle',
---
---		error = 'love',
---		hint = 'iris',
---		info = 'foam',
---		warn = 'gold',
---
---		headings = {
---			h1 = 'iris',
---			h2 = 'foam',
---			h3 = 'rose',
---			h4 = 'gold',
---			h5 = 'pine',
---			h6 = 'foam',
---		}
---		-- or set all headings at once
---		-- headings = 'subtle'
---	},
---
---	-- Change specific vim highlight groups
---	-- https://github.com/rose-pine/neovim/wiki/Recipes
---	highlight_groups = {
---		ColorColumn = { bg = 'rose' },
---
---		-- Blend colours against the "base" background
---		CursorLine = { bg = 'rose', blend = 10 },
---		StatusLine = { fg = 'love', bg = 'love', blend = 3 },
---	}
---})
+local c = require('vscode.colors').get_colors()
 
+require('vscode').setup({
+    -- Alternatively set style in setup
+    -- style = 'light'
 
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = true,
+
+    -- Underline `@markup.link.*` variants
+    underline_links = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = true,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        --vscLineNumber = '#FFFFFF',
+    },
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+    }
+})
 
 
 
